46f0c870bc55a7bcfb5003c6e096f2b75476174d
Morné Chamberlain <thefreeman.za@gmail.com>
2013-11-17 01:36:46 +0200

    MOAI_WITH_EXT_COMPONENTS set up in Android build.
    MOAI_WITH_EXT_COMPONENTS is now defined if MOAI_COMPONENTS is
    defined/used in the Android build.
    
    The include directory of MOAI_COMPONENTS is now also added to
    MY_HEADER_SEARCH_PATHS in the Android build scripts.

--- ant/libmoai/jni/moaicore/Android.mk
+++ ant/libmoai/jni/moaicore/Android.mk
@@ -9,6 +9,9 @@
 	LOCAL_MODULE 		:= moaicore
 	LOCAL_ARM_MODE 		:= $(MY_ARM_MODE)
 	LOCAL_CFLAGS		:= -include $(MY_MOAI_ROOT)/src/zl-vfs/zl_replace.h
+	ifdef MOAI_COMPONENTS
+		LOCAL_CFLAGS	+= -DMOAI_WITH_EXT_COMPONENTS=1
+	endif
 
 	LOCAL_C_INCLUDES 	:= $(MY_HEADER_SEARCH_PATHS)
 	LOCAL_SRC_FILES 	+= $(wildcard $(MY_MOAI_ROOT)/src/moai-core/*.cpp) 
